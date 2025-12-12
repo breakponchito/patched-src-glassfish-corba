d8a3622887b7238a2ccdb8e28d9bb4b17a16c92b
Alfonso Altamirano <alfonso.altamirano@payara.fish>
2025-12-11 21:12:49 -0600

    FISH-12402: adding missed copyrights

--- internal-api/src/main/java/module-info.java
+++ internal-api/src/main/java/module-info.java
@@ -13,6 +13,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
+// Portions Copyright [2025] [Payara Foundation and/or its affiliates]
 
 module org.glassfish.corba.internal {
 
